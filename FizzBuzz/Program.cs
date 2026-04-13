dc9b622aa47be7936175cbc4c0e72d0c6c80193c
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-13 18:52:45 +0200

    docs(FizzBuzz): add pseudocode for FizzBuzz implementation

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,8 @@
-// See https://aka.ms/new-console-template for more information
+// PSEUDOCODE
 
-Console.WriteLine("Hello, World!");
+// Read "X Y N" from a single line of input
+// For each integer I from 1 to N:
+//   if I divisible by both X and Y → print "FizzBuzz"
+//   else if I divisible by X       → print "Fizz"
+//   else if I divisible by Y       → print "Buzz"
+//   else                           → print I
