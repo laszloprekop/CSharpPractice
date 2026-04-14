06159779bc406cff72b8234d00fdc2ebf8b729fd
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-14 09:15:58 +0200

    docs(FizzBuzz): add test case examples

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,8 +1,23 @@
-// PSEUDOCODE
+/*
+ * Test cases:
+ * "2 3 7" -> 1, Fizz, Buzz, Fizz, 5, FizzBuzz, 7
+ * "2 4 7" -> 1, Fizz, 3, FizzBuzz, 5, Fizz, 7
+ * "3 5 7" -> 1, 2, Buzz, 4, Buzz, Fizz, 7
+ *
+ * "1 1 5" -> FizzBuzz, FizzBuzz, FizzBuzz, FizzBuzz, FizzBuzz - always both true
+ * "5 7 1" -> 1 - no divisibility
+ */
 
-// Read "X Y N" from a single line of input
-// For each integer I from 1 to N:
-//   if I divisible by both X and Y → print "FizzBuzz"
-//   else if I divisible by X       → print "Fizz"
-//   else if I divisible by Y       → print "Buzz"
-//   else                           → print I
+
+/*
+ * PSEUDOCODE
+ *
+ * Read "X Y N" from a single line of input
+ *   For each integer I from 1 to N:
+ *   if I divisible by both X and Y → print "FizzBuzz"
+ *   else if I divisible by X       → print "Fizz"
+ *   else if I divisible by Y       → print "Buzz"
+ *   else                           → print I
+*/
+
+
