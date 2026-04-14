80d1ee9501a98657ac5db1dcbe8b086fd275cb5c
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-14 11:57:40 +0200

    feat(tutorial): add random answer generation

--- TutorialTopLevelStatements/Program.cs
+++ TutorialTopLevelStatements/Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+
 Console.WriteLine();
+
+/*
+Display as Strings of arguments
 
 foreach (var s in args)
 {
@@ -8,3 +13,12 @@ foreach (var s in args)
     Console.WriteLine(' ');
 }
 Console.WriteLine();
+
+*/
+
+// Display a random answer
+
+string[] answers = [ "Yes", "No", "Maybe" ];
+
+var index = new Random().Next(answers.Length-1);
+Console.WriteLine(answers[index]);
