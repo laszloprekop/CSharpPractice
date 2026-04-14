9874fbdad4d0e8c92de3040a7d1bf946d8311bac
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-14 16:17:20 +0200

    feat(tutorial): testing out some ASCII animation

--- TutorialTopLevelStatements/Program.cs
+++ TutorialTopLevelStatements/Program.cs
@@ -3,7 +3,6 @@
 using System.Diagnostics;
 
 // Console.WriteLine();
-
 /*
 // Respond as Strings of arguments
 
@@ -15,12 +14,36 @@ foreach (var s in args)
 Console.WriteLine();
 
 */
-
-
-
+/*
 // Respond with a random answer
 
 string[] answers = [ "Yes", "No", "Maybe" ];
+
+var index = new Random().Next(answers.Length);
+Console.WriteLine(answers[index]);
+*/
+
+// testing out some ASCII animation
+using System.Threading.Tasks;
+
+const int delay = 100;
+for (var i = 0; i < 10; i++)
+{
+    Console.Write("|");
+    await Task.Delay(delay);
+    Console.Write("\b");
+    Console.Write("/");
+    await Task.Delay(delay);
+    Console.Write("\b");
+    Console.Write("-");
+    await Task.Delay(delay);
+    Console.Write("\b");
+    Console.Write("\\");
+    await Task.Delay(delay);
+    Console.Write("\b");
+}
+
+string[] answers = ["This is positive", "This is negative", "Rather not tell", "You tell me"];
 
 var index = new Random().Next(answers.Length);
 Console.WriteLine(answers[index]);
