ab56d80598585d6999771626b5f5a6cdcadc7d57
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-16 21:17:10 +0200

    fix(PIM): correct typo in css classname

--- PolisIncidentMap/frontend/src/components/IncidentCard.jsx
+++ PolisIncidentMap/frontend/src/components/IncidentCard.jsx
@@ -8,7 +8,7 @@ function IncidentCard({incident}) {
   return (
     <a href={`https://polisen.se${incident.url}`}
        target="_blank"
-       className="block p-3 mb-2 rounded border border-gray-200 hower:border-blue-400 hover:shadow transition-all">
+       className="block p-3 mb-2 rounded border border-gray-200 hover:border-blue-400 hover:shadow transition-all">
       <div className="flex items-center justify-between mb-1">
         <span
           className="text-xs font-semibold bg-blue-100 text-blue-800 px-2 py-0.5 rounded-full">{incident.type}</span>
