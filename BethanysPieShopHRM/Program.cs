0128ab4fe78e2da905338661feb83133c3f746a8
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-27 16:45:10 +0200

    feat(PSCSF): add examples of explicit and implicit typing

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -24,3 +24,19 @@ int intVeryLongDay = (int)veryLongDay;
 Console.WriteLine($"A very long day as an integer: {intVeryLongDay}");
 
 Console.ReadLine();
+
+/*
+ * Explicit typing: You explicitly declare the type of the variable
+ */
+
+int k = 123;
+bool l = true;
+double m = 11.2;
+
+/*
+ * Implicit typing: C# figures out the type of the variable based on the value assigned to it.
+ */
+
+var n = 123; // n will be an integer
+var o = false; // o will be a bool
+var p = 11.31415; // p will be a double  
