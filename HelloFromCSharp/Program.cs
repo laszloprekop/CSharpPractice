30c77d422ab2b4690cabb60bdd85198ca97c305c
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-27 14:32:10 +0200

    feat(PSCSF): add user input for name

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello everybody");
+
+Console.WriteLine("Please enter your name:");
+
+string name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
