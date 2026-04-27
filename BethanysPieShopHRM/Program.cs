1a17956a540a96abd5336a9a23c98582816c7fa2
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-27 16:24:34 +0200

    feat(PSCSF): add examples of date and time calculations

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿DateTime hireDate = new DateTime(2026, 2, 20, 16, 2, 0);
+
+Console.WriteLine(hireDate);
+
+DateTime exitDate = new DateTime(2026, 4, 27, 16, 20, 0);
+
+TimeSpan workPeriod = exitDate - hireDate;
+Console.WriteLine($"Days: {workPeriod.Days}");
+Console.WriteLine($"Total Days: {workPeriod.TotalDays}");
