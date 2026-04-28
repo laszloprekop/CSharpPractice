1440154e1c256dae724ea30cf2921bbd84d8fe71
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 09:09:44 +0200

    feat(PSCSF): add new age validation logic using if-else statement

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Enter the age of the new candidate:");
+int age = int.Parse(Console.ReadLine());
+
+if (age < 18)
+{
+    Console.WriteLine("Too young to apply.");
+    Console.WriteLine("Send email to candidate.");
+}
+else
+{
+    Console.WriteLine("Great, you can now start with the application.");
+}
