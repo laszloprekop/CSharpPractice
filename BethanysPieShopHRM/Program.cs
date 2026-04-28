b439cf6be3929b24b8fe0e84e1bbcc73bdc408ea
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 09:58:35 +0200

    feat(PSCSF): add while loop examples for counting up and down

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿/*
+ * While loop example
+ */
+
+Console.Write("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+int count = 0;
+while (count < max)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+Console.WriteLine($"So this is how we reached {max}");
+
+
+Console.WriteLine("Enter another value:");
+int max2 = int.Parse(Console.ReadLine());
+int count2 = max2;
+while (count2 > 0)
+{
+    Console.WriteLine(count2);
+    count2--;
+}
+
+Console.WriteLine($"So this is how we reached 0 from {max2}");
